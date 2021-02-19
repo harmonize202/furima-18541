7d05d1abffe0812ec4544e0fca8d6ee7185cf650
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-19 20:45:06 +0900

    バリデーション条件追加・可読性向上
    with_optionsでバリデーションをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,21 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  # 全角(漢字・ひらがな・カタカナ)のみの入力
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :last_name,       presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
+  # 全角(漢字・ひらがな・カタカナ)、かつ「々」の入力
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々]+\z/
+  # with_optionsメソッドでバリデーションをまとめる
+  with_options presence: true, format: { with: VALID_NAME_REGEX } do
+    validates :last_name
+    validates :first_name
+  end
 
   # 全角(カタカナ)のみの入力
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :last_name_kana,  presence: true, format: { with: VALID_NAME_KANA_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
-
+  # with_optionsメソッドでバリデーションをまとめる
+  with_options presence: true, format: { with: VALID_NAME_KANA_REGEX } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  
   validates :birthday,        presence: true
 end
