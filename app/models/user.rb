bd860ddb9abfdbb76abdcf34f63e7968dac2aad6
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-19 18:44:21 +0900

    新規登録時のテストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 半角英数字(大文字・小文字)のみの入力(passwordにバリデーションをかける)
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+
+  # 全角(漢字・ひらがな・カタカナ)のみの入力
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name,       presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
+
+  # 全角(カタカナ)のみの入力
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana,  presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+
   validates :birthday,        presence: true
 
 end
