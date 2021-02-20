707c451e1b513a52d9088bcb5e150a7525fc94eb
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-20 21:38:42 +0900

    「出品する」ボタンから購入画面へ遷移する実装
    itemsコントローラーのnewアクションへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # ホームページ(http://localhost:3000/)はitemsコントローラーのindexアクションへ飛ぶ。
   root to: "items#index" 
+  resources :items, only: [:index, :new, :create]
 end
