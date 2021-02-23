6e4d756e4a7f0aa8fd1e99485e754138694d6d51
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-23 20:38:00 +0900

    価格計算表示機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+function item_price(){
+// idが"item-price"のオブジェクトをpriceへ格納
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  const fee = document.getElementById("add-tax-price")
+  fee.innerHTML = Math.floor( inputValue * 0.1 )
+  const profit = document.getElementById("profit")
+  profit.innerHTML = Math.floor( inputValue * 0.9 )
+})
+}
+window.addEventListener("load",item_price)
