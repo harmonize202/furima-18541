2a5c75ce17ed9697a1253562c6d97a2ec3779292
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-24 16:23:01 +0900

    rubocop実施

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '販売価格が10,000,000円以上の場合に出品ができないこと' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
