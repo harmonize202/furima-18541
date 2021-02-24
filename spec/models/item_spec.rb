4af23fd44c714f791b1bab7207b1089deff3e9ff
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-24 15:17:27 +0900

    レビュー対応1-2
    active_hashのバリデーション設定のテスト記述追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,33 +21,33 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーの情報が必須であること' do
-        @item.category_id = ''
+        @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category is not a number')
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
 
       it '商品の状態についての情報が必須であること' do
-        @item.status_id = ''
+        @item.status_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Status is not a number')
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
 
       it '配送料の負担についての情報が必須であること' do
-        @item.delivery_fee_id = ''
+        @item.delivery_fee_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery fee is not a number')
+        expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
       end
 
       it '発送元の地域についての情報が必須であること' do
-        @item.shipment_source_id = ''
+        @item.shipment_source_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipment source is not a number')
+        expect(@item.errors.full_messages).to include('Shipment source must be other than 1')
       end
 
       it '発送までの日数についての情報が必須であること' do
-        @item.shipment_day_id = ''
+        @item.shipment_day_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipment day is not a number')
+        expect(@item.errors.full_messages).to include('Shipment day must be other than 1')
       end
 
       it '販売価格についての情報が必須であること' do
