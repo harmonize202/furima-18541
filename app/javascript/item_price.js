eb81f32d9f5ea82c745bbc6885af5f544f1b5b26
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-24 15:06:00 +0900

    レビュー対応1-1
    ・コメントアウト記述削除
    ・コントローラー側でのログイン有無での強制遷移設定
    ・可読性向上

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,6 @@ function item_price(){
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
   const fee = document.getElementById("add-tax-price")
   fee.innerHTML = Math.floor( inputValue * 0.1 )
   const profit = document.getElementById("profit")
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,11 @@ class Item < ApplicationRecord
   belongs_to :shipment_source
   belongs_to :shipment_day
 
-  validates :item_name,   presence: true
-  validates :explanation, presence: true
-  validates :image,       presence: true
+  with_options presence: true do
+    validates :item_name
+    validates :explanation
+    validates :image
+  end
 
   # 「価格」は半角数字のみ、かつ、の入力
   VALID_PRICE_REGEX = /\A[0-9]+\z/
@@ -21,9 +23,11 @@ class Item < ApplicationRecord
                       numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   # ジャンルの選択が「---」(１つ目の選択肢)の時は保存できないようにする。バリデーションの１種。
-  validates :category_id,        numericality: { other_than: 1 }
-  validates :status_id,          numericality: { other_than: 1 }
-  validates :delivery_fee_id,    numericality: { other_than: 1 }
-  validates :shipment_source_id, numericality: { other_than: 1 }
-  validates :shipment_day_id,    numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :shipment_source_id
+    validates :shipment_day_id
+  end
 end
