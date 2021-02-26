45aea226dd24581160f9ba85a9d3a6eaf98acf6d
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-26 22:02:36 +0900

    レビュー対応1-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :item_data_input, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,17 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
     # editアクションで model: @item の為、変数は @item と設定
-    @item = Item.find(params[:id])
     if @item.update(items_params)
       redirect_to root_path
     else
@@ -45,4 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :delivery_fee_id, :shipment_source_id,
                                  :shipment_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def item_data_input
+    @item = Item.find(params[:id])
+  end
+
 end
