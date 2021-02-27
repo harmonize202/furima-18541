b02da30d13ca2e7cafa09739904b6f0448bead81
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-27 18:05:20 +0900

    出品した商品の削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :item_data_input, only: [:show, :edit, :update]
+  before_action :item_data_input, only: [:show, :edit, :update, :destroy]
   before_action :signed_user, only: [:edit, :update]
 
   def index
@@ -35,6 +35,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # ホームページ(http://localhost:3000/)はitemsコントローラーのindexアクションへ飛ぶ。
   root to: "items#index" 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :items
 end
